27c359870e5ed3932345273e2d15ae2073d1de53
Wayne Koorts <wayne@wkoorts.com>
2026-03-14 23:05:39 +1300

    Add first document controller endpoint

--- src/backend/jargoyle-web/src/main/java/com/jargoyle/controller/DocumentController.java
+++ src/backend/jargoyle-web/src/main/java/com/jargoyle/controller/DocumentController.java
@@ -1,15 +1,39 @@
 package com.jargoyle.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jargoyle.service.security.AuthenticatedUserResolver;
+import com.jargoyle.dto.DocumentListResponse;
+import com.jargoyle.entity.User;
+import com.jargoyle.service.DocumentService;
 
 @RestController
 @RequestMapping("/api/documents")
 public class DocumentController {
 
-    //private final AuthenticatedUserResolver authenticatedUserResolver;
-    //private final DocumentServi
+    private final DocumentService documentService;
+
+    public DocumentController(
+        DocumentService documentService) {
+
+        this.documentService = documentService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<DocumentListResponse>> list(
+        @CurrentUser User user,
+        Pageable pageable) {
+
+        var docList = documentService.list(user.getId(), pageable);
+
+        return ResponseEntity.ok(docList);
+    }
+
+
+
 
 }
